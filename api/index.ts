356d04c0744e9d6c3bd78254b9b2e8099b5634bc
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2026-04-24 22:34:31 +0700

    feat: restructure API setup by moving server initialization to src/index.ts and updating tsconfig.json

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,3 @@
-import express from "express";
-import cors from "cors";
-import router from "../src/routes/index.route";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-router(app)
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+import app from "../src";
 
 export default app;
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.route";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+router(app)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
